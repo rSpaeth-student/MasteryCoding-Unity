47bfbbe12d277536d3705d371c799faf20aef97d
rSpaeth-student <rs1113297@student.francistuttle.edu>
2025-09-17 10:50:04 -0500

    Recent update

--- Unit 8/Grayboxing with ProBuilder/Assets/Scripts/Card.cs
+++ Unit 8/Grayboxing with ProBuilder/Assets/Scripts/Card.cs
@@ -1,16 +1,18 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Card : MonoBehaviour
 {
     private Player player;
     private Vector3 direction;
-    private float speed = 10f;
+    private float speed = 20f;
     public float duration = 5f;
 
-    public void Setup(Vector3 dir, Player player)
+    public void Setup(Vector3 dir, Player player, float duration)
     {
         this.player = player;
         direction = dir.normalized;
+        this.duration = duration;
         Destroy(gameObject, duration); // 
     }
 
@@ -18,7 +20,7 @@ public class Card : MonoBehaviour
     {
         transform.position += direction * speed * Time.deltaTime;
     }
-    
+
 
 
     private void OnTriggerEnter(Collider collider)
@@ -27,7 +29,10 @@ public class Card : MonoBehaviour
         {
             player.GetMonsters().Add(monster); //passing the reference
             player.DisplayMonsters();
+            monster.gameObject.SetActive(false);
             Destroy(gameObject);
         }
     }
+
+    
 }
--- Unit 8/Grayboxing with ProBuilder/Assets/Scripts/Player.cs
+++ Unit 8/Grayboxing with ProBuilder/Assets/Scripts/Player.cs
@@ -1,25 +1,33 @@
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using System.Collections;
 
 public class Player : MonoBehaviour
 {
+    public Vector3 offset = new Vector3(3,0,0);
     private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            SummonMonster();
+        }
     }
     [SerializeField] private LineRenderer tracerPrefab;
     private List<Monster> monsters = new List<Monster>();
     [SerializeField] LayerMask layerMask;
     [SerializeField] private GameObject launchPosition;
     [SerializeField] private GameObject cardPrefab;
+    private bool isMonsterSummoned = false;
+    private Transform playerTransform;
 
-    private void Shoot(Vector3 shootPosition, Vector3 targetPosition)
+    private void Shoot(Vector3 shootPosition, Vector3 targetPosition, float duration)
     {
         GameObject card = Instantiate(cardPrefab, shootPosition, Quaternion.identity);
         Vector3 shootDirection = (targetPosition - shootPosition).normalized;
 
-        card.GetComponent<Card>().Setup(shootDirection, this);
+
+        card.GetComponent<Card>().Setup(shootDirection, this, duration);
     }
 
     public List<Monster> GetMonsters()
@@ -35,12 +43,12 @@ public class Player : MonoBehaviour
         }
 
     }
-    
+
     void Awake()
     {
         layerMask = LayerMask.GetMask("Wall", "Character");
     }
-    
+
     // See Order of Execution for Event Functions for information on FixedUpdate() and Update() related to physics queries
     void FixedUpdate()
     {
@@ -58,7 +66,7 @@ public class Player : MonoBehaviour
             {
                 Debug.DrawRay(launchPosition.transform.position, launchPosition.transform.TransformDirection(Vector3.forward) * hit.distance, Color.green);
                 Debug.Log("Did Hit");
-                Shoot(launchPosition.transform.position, hit.transform.position);
+                Shoot(launchPosition.transform.position, hit.transform.position, 5f);
             }
             else
             {
@@ -67,5 +75,42 @@ public class Player : MonoBehaviour
             }
         }
 
+    }
+
+    private void SummonMonster()
+    {
+
+        if (isMonsterSummoned) return;
+
+        StartCoroutine(cardRoutine());
+    }
+
+    private IEnumerator cardRoutine()
+    {
+        isMonsterSummoned = true;
+        //get player transform
+        
+        
+        
+        playerTransform = GetComponent<Transform>();
+        Debug.Log(playerTransform.position);
+
+        playerTransform.position += offset; //offset player transform
+        Debug.Log(playerTransform.position);
+
+        Shoot(launchPosition.transform.position, playerTransform.position + offset, 1f);
+
+        //shoot card at offset transform
+        yield return new WaitForSeconds(1);
+
+        monsters[0].gameObject.SetActive(true);
+        //change monster transform to players offset transform
+        monsters[0].gameObject.transform.position = playerTransform.position + offset;
+        
+
+       // monsters[0].gameObject.SetActive(true);
+
+        //summon monster
+        isMonsterSummoned = false;
     }
 }
